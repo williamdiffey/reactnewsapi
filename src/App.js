02c8552556c4693fd2334f4b744600e303fe17c7
William Diffey <william.diffey@protonmail.com>
2019-08-06 22:35:37 +0900

    modified basic fetch src/App.js 	removed boilerplate from src/index.js 	deleted: src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      index: 9,
+      title: [],
+      urlToImage: [],
+      description: [],
+      url: [],
+    }
+  }
+
+  
+  componentDidMount() {
+    fetch("https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=ac5a319dd1594827817faa8cba97a333")
+    .then(response => response.json())
+    .then(data => {
+      // makes array of objects and discards the crap data
+      const articles = data.articles;
+      // console.log(articles)
+      
+      // interate over array to extract key/value pairs
+      const title = Object.keys(articles)
+        .map(key => articles[key].title)
+        // console.log(title)
+        this.setState({
+          title
+        });
+
+      const urlToImage = Object.keys(articles)
+        .map(key => articles[key].urlToImage)
+        // console.log(urlToImage)
+        this.setState({
+          urlToImage
+        });
+
+      const description = Object.keys(articles)
+        .map(key => articles[key].description)
+        // console.log(description)
+        this.setState({
+          description
+        });
+
+      const url = Object.keys(articles)
+        .map(key => articles[key].url)
+        // console.log(url)
+        this.setState({
+          url
+        });
+    })
+    }
+
+
+     
+
+  render() {
+  
+    // this all needs to go and live in Card.js
+    return (
+      <div>
+        <ul>
+          <li>{this.state.title[this.state.index]}</li>
+          <li>{this.state.urlToImage[this.state.index]}</li>
+          <li>{this.state.urlToImage[this.state.index]}</li>
+          <li>{this.state.url[this.state.index]}</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
